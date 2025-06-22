98c36faad682355d9708ff5700a0750432da9afb
xirzo <easttem@gmail.com>
2025-06-23 01:59:00 +0300

    chore: export commands

--- bin/main.c
+++ bin/main.c
@@ -1,11 +1,28 @@
 #include <unistd.h>
 #include "dv.h"
 
+/* Result I wanna get: */
+
+/*
+.
+├── bin
+│   └── main
+├── include
+│   └── GLFW
+│       ├── glfw3.h
+│       └── glfw3native.h
+├── lib
+│   └── libglfw3.a
+└── src
+    └── main.c
+*/
+
 int main(int argc, char *argv[]) {
     if (argc == 1) {
         fsize(".");
     } else {
         while (--argc > 0) { fsize(*++argv); }
     }
+
     return 0;
 }
